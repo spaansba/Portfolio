85402a56a223217cf24b1880af7db8d5f03b639d
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2025-03-31 15:03:56 +0200

    timeline v2 included reversed timeline

--- src/app/about/WorkExperience/Timeline.tsx
+++ src/app/about/WorkExperience/Timeline.tsx
@@ -8,23 +8,12 @@ const Timeline = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>
 Timeline.displayName = "Timeline"
 
 const TimelineItem = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("group relative pb-8 pl-8 sm:pl-44", className)} {...props} />
-  )
+  ({ className, ...props }, ref) => <div ref={ref} className={cn("", className)} {...props} />
 )
 TimelineItem.displayName = "TimelineItem"
 
 const TimelineHeader = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn(
-        "mb-1 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-slate-300 before:px-px after:absolute after:left-2 after:box-content after:h-2 after:w-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-4 after:border-primary-foreground/95 after:bg-foreground group-last:before:hidden sm:flex-row sm:before:left-0 sm:before:ml-[10rem] sm:after:left-0 sm:after:ml-[10rem]",
-        className
-      )}
-      {...props}
-    />
-  )
+  ({ className, ...props }, ref) => <div ref={ref} className={cn(className)} {...props} />
 )
 TimelineHeader.displayName = "TimelineHeader"
 
@@ -41,14 +30,7 @@ const TimelineTime = ({
   ...props
 }: React.ComponentProps<typeof Badge>) => {
   return (
-    <Badge
-      className={cn(
-        "left-[-5px] mb-3 px-2 py-1 inline-flex h-6 w-36 translate-y-0.5 items-center justify-center text-xs font-semibold uppercase sm:absolute sm:mb-0",
-        className
-      )}
-      variant={variant}
-      {...props}
-    >
+    <Badge className={cn("", className)} variant={variant} {...props}>
       {props.children}
     </Badge>
   )
