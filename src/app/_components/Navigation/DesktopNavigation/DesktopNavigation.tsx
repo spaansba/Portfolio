42a19a8b41af0a05c4ef519096d7a1f7b9b201e0
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2025-04-08 15:08:24 +0200

    trying to fix issue in productive

--- src/app/_components/Navigation/DesktopNavigation/DesktopNavigation.tsx
+++ src/app/_components/Navigation/DesktopNavigation/DesktopNavigation.tsx
@@ -15,8 +15,8 @@ function DesktopNavigation() {
         <motion.div
           layout
           initial={false}
-          className={`flex flex-col bg-SecondaryGray h-[calc(100vh-73px)]`}
-          animate={{ width: isSidebarOpen ? SIDEBAR_OPEN_WIDTH : SIDEBAR_CLOSED_WIDTH }}
+          className={`flex flex-col bg-SecondaryGray `}
+          // animate={{ width: isSidebarOpen ? SIDEBAR_OPEN_WIDTH : SIDEBAR_CLOSED_WIDTH }}
           transition={{ duration: 0.4, ease: "easeInOut" }}
         >
           <div className="ml-[10px] overflow-x-hidden scrollbar-hide h-full">
