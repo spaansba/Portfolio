44b68160ceb2904cbe51160efea0e160193e55bd
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2025-04-09 09:49:10 +0200

    testing in prod3

--- src/hooks/useScrollToHash.ts
+++ src/hooks/useScrollToHash.ts
@@ -6,60 +6,60 @@ export const useScrollToHash = () => {
   const navigationActions = useNavigationActions()
   const [isScrolling, setIsScrolling] = useState(false)
   const isMobile = useIsMobileDevice()
-  useEffect(() => {
-    const scrollTo = localStorage.getItem("scrollToHash")
-    if (!scrollTo) {
-      return
-    }
-    setIsScrolling(true)
-    const timeoutId = setTimeout(() => {
-      const targetElement = document.querySelector(scrollTo)
+  // useEffect(() => {
+  //   const scrollTo = localStorage.getItem("scrollToHash")
+  //   if (!scrollTo) {
+  //     return
+  //   }
+  //   setIsScrolling(true)
+  //   const timeoutId = setTimeout(() => {
+  //     const targetElement = document.querySelector(scrollTo)
 
-      if (targetElement) {
-        targetElement.scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        })
-      }
-      localStorage.removeItem("scrollToHash")
-      setTimeout(() => {
-        setIsScrolling(false)
-      }, 900)
-    }, 300)
+  //     if (targetElement) {
+  //       targetElement.scrollIntoView({
+  //         behavior: "smooth",
+  //         block: "start",
+  //       })
+  //     }
+  //     localStorage.removeItem("scrollToHash")
+  //     setTimeout(() => {
+  //       setIsScrolling(false)
+  //     }, 900)
+  //   }, 300)
 
-    return () => clearTimeout(timeoutId)
-  }, [])
+  //   return () => clearTimeout(timeoutId)
+  // }, [])
 
-  useEffect(() => {
-    if (typeof window === "undefined") return
+  // useEffect(() => {
+  //   if (typeof window === "undefined") return
 
-    const sections = document.querySelectorAll("[id]")
+  //   const sections = document.querySelectorAll("[id]")
 
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting && !isScrolling) {
-            const id = entry.target.id
-            const page = navigationActions.getPageBasedOnHash(`#${id}`)
-            navigationActions.setSelectedPage(page)
-          }
-        })
-      },
-      {
-        root: null,
-        rootMargin: isMobile ? "500px 0px -70% 0px" : "-400px 0px -99% 0px",
-        threshold: 0,
-      }
-    )
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.isIntersecting && !isScrolling) {
+  //           const id = entry.target.id
+  //           const page = navigationActions.getPageBasedOnHash(`#${id}`)
+  //           navigationActions.setSelectedPage(page)
+  //         }
+  //       })
+  //     },
+  //     {
+  //       root: null,
+  //       rootMargin: isMobile ? "500px 0px -70% 0px" : "-400px 0px -99% 0px",
+  //       threshold: 0,
+  //     }
+  //   )
 
-    sections.forEach((section) => {
-      observer.observe(section)
-    })
+  //   sections.forEach((section) => {
+  //     observer.observe(section)
+  //   })
 
-    return () => {
-      sections.forEach((section) => {
-        observer.unobserve(section)
-      })
-    }
-  }, [isScrolling, navigationActions, isMobile])
+  //   return () => {
+  //     sections.forEach((section) => {
+  //       observer.unobserve(section)
+  //     })
+  //   }
+  // }, [isScrolling, navigationActions, isMobile])
 }
