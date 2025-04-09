84cb2f377eb16be43b8deddf5a982f338d35554a
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2025-04-09 10:48:45 +0200

    testing in prop17

--- src/app/_components/Navigation/DesktopNavigation/DesktopNavigation.tsx
+++ src/app/_components/Navigation/DesktopNavigation/DesktopNavigation.tsx
@@ -12,18 +12,18 @@ function DesktopNavigation() {
 
   return (
     <nav className="h-full flex">
-      <div
-        // layout
-        // initial={false}
+      <motion.div
+        layout
+        initial={false}
         className="bg-SecondaryGray h-full overflow-hidden"
         style={{ width: isSidebarOpen ? SIDEBAR_OPEN_WIDTH : SIDEBAR_CLOSED_WIDTH }}
-        // animate={{ width: isSidebarOpen ? SIDEBAR_OPEN_WIDTH : SIDEBAR_CLOSED_WIDTH }}
-        // transition={{ duration: 0.4, ease: "easeInOut" }}
+        animate={{ width: isSidebarOpen ? SIDEBAR_OPEN_WIDTH : SIDEBAR_CLOSED_WIDTH }}
+        transition={{ duration: 0.4, ease: "easeInOut" }}
       >
         <div className="ml-[10px] h-full relative">
           <Navigation />
         </div>
-      </div>
+      </motion.div>
       <SidebarToggleButton />
     </nav>
   )
