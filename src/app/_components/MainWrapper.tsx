3789a6057800a7e12b2b1c97d7e8a3d7c624ecc3
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2025-04-11 15:12:59 +0200

    fix

--- src/app/_components/MainWrapper.tsx
+++ src/app/_components/MainWrapper.tsx
@@ -7,26 +7,28 @@ import MobileNavigationWrapper from "./navigation/mobileNavigation/MobileNavigat
 
 async function MainWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen overflow-hidden">
-      <header className="bg-SecondaryGray fixed top-0 right-0 left-0 z-30">
-        <PageHeader>
-          <ProfileInfo />
-        </PageHeader>
-      </header>
+    <>
+      <div className="h-screen overflow-hidden">
+        <header className="bg-SecondaryGray fixed top-0 right-0 left-0 z-30">
+          <PageHeader>
+            <ProfileInfo />
+          </PageHeader>
+        </header>
 
-      <div className="mt-[73px] grid h-[calc(100vh-73px)] grid-cols-[auto_1fr] overflow-hidden">
-        <div className="hidden md:block">
-          <DesktopNavigationWrapper />
-        </div>
-        <main id="contentwrapper" className="scrollbar-custom overflow-auto">
-          {children}
-          <ContentFooter />
-        </main>
-        <div className="absolute right-0 bottom-0 left-0 md:hidden">
-          <MobileNavigationWrapper />
+        <div className="mt-[73px] grid h-[calc(100vh-73px)] grid-cols-[auto_1fr] overflow-hidden">
+          <div className="hidden md:block">
+            <DesktopNavigationWrapper />
+          </div>
+          <main id="contentwrapper" className="scrollbar-custom overflow-auto">
+            {children}
+            <ContentFooter />
+          </main>
+          <div className="absolute right-0 bottom-0 left-0 md:hidden">
+            <MobileNavigationWrapper />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
