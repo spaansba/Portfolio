ff242f8e4c0c27783cbdd4c5965303c63355707b
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2025-04-24 11:22:31 +0200

    transition animation added to read more button on big projects

--- src/app/about/ProjectList/BigProjects/BigProjectDescription.tsx
+++ src/app/about/ProjectList/BigProjects/BigProjectDescription.tsx
@@ -9,30 +9,31 @@ type BigProjectDescriptionProps = {
 function BigProjectDescription({ description }: BigProjectDescriptionProps) {
   const [showButton] = useState(description.length > 1);
   const [expanded, setExpanded] = useState(false);
+
   return (
     <div className="text-white">
       <p className="text-base">{description[0]}</p>
 
       {showButton && (
-        <>
-          {expanded && (
-            <>
-              {description.slice(1).map((paragraph, index) => (
-                <p key={index} className="mt-2 text-base">
-                  {paragraph}
-                </p>
-              ))}
-            </>
-          )}
+        <div
+          className={`transition-all ease-in-out ${expanded ? "max-h-[1000px] opacity-100 duration-500" : "max-h-0 overflow-hidden opacity-0 duration-300"}`}
+        >
+          {description.slice(1).map((paragraph, index) => (
+            <p key={index} className="mt-2 text-base">
+              {paragraph}
+            </p>
+          ))}
+        </div>
+      )}
 
-          <FastButton
-            onClick={() => setExpanded(!expanded)}
-            className="text-fgButton hover:text-fgButtonHover mt-2 cursor-pointer text-sm font-medium focus:outline-none"
-            aria-label={`${expanded ? "Show Less" : "Read More"}`}
-          >
-            {expanded ? "Show Less" : "Read More"}
-          </FastButton>
-        </>
+      {showButton && (
+        <FastButton
+          onClick={() => setExpanded(!expanded)}
+          className="text-fgButton hover:text-fgButtonHover mt-2 cursor-pointer text-sm font-medium focus:outline-none"
+          aria-label={`${expanded ? "Show Less" : "Read More"}`}
+        >
+          {expanded ? "Show Less" : "Read More"}
+        </FastButton>
       )}
     </div>
   );
